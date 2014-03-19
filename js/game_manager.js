5d33031759273b0a8c3a8e628dd884ed34e24925
sszuflita <sam.szuflita@gmail.com>
2014-03-19 12:17:14 +0000

    can move up

--- js/game_manager.js
+++ js/game_manager.js
@@ -155,7 +155,7 @@ GameManager.prototype.move = function (direction) {
   if (moved) {
     // our target
     this.addRandomTile();
-      console.log(this.movesAvailable);
+      console.log(this.movesAvailable());
 
     if (!this.movesAvailable()) {
       this.over = true; // Game over!
